8d738de1e389c79f0d0da2dc16dbe92d95bfc937
Tom <tom@trailmarker.io>
2022-12-05 21:26:22 +0930

    solution for Day 2 Part 2

--- 2/solution.ts
+++ 2/solution.ts
@@ -1,3 +1,5 @@
+import { SocketAddress } from "net";
+
 // Items in the game
 enum Item {
     Rock = 1,
@@ -5,38 +7,76 @@ enum Item {
     Scissors = 3
 }
 
-// Decode 1up and 2up's moves into Items
+enum Score {
+    Lose = 0,
+    Draw = 1,
+    Win = 2
+}
+
+// Decode 1up and 2up's moves (or required score) into Items (or a Score)
 const p1Item = (move: string ): Item => <Item>(move.charCodeAt(0) - 'A'.charCodeAt(0) + 1); 
 const p2Item = (move: string ): Item => <Item>(move.charCodeAt(0) - 'X'.charCodeAt(0) + 1);
+const p2Score = (score: string): Score => <Score>(score.charCodeAt(0) - 'X'.charCodeAt(0));
 
 // Scores for possible results
-const SCORES = {
+const PART1_SCORES = {
     [Item.Rock]: {
-        [Item.Rock]: 3,
-        [Item.Paper]: 6,
-        [Item.Scissors]: 0
+        [Item.Rock]: Score.Draw,
+        [Item.Paper]: Score.Win,
+        [Item.Scissors]: Score.Lose
     },
     [Item.Paper]: {
-        [Item.Rock]: 0,
-        [Item.Paper]: 3,
-        [Item.Scissors]: 6
+        [Item.Rock]: Score.Lose,
+        [Item.Paper]: Score.Draw,
+        [Item.Scissors]: Score.Win
     },
     [Item.Scissors]: {
-        [Item.Rock]: 6,
-        [Item.Paper]: 0,
-        [Item.Scissors]: 3
+        [Item.Rock]: Score.Win,
+        [Item.Paper]: Score.Lose,
+        [Item.Scissors]: Score.Draw
     },
 };
+
+const PART2_ITEMS = {
+    [Item.Rock]: {
+        [Score.Lose]: Item.Scissors,
+        [Score.Draw]: Item.Rock,
+        [Score.Win]: Item.Paper
+    },
+    [Item.Paper]: {
+        [Score.Lose]: Item.Rock,
+        [Score.Draw]: Item.Paper,
+        [Score.Win]: Item.Scissors
+    },
+    [Item.Scissors]: {
+        [Score.Lose]: Item.Paper,
+        [Score.Draw]: Item.Scissors,
+        [Score.Win]: Item.Rock
+    }
+};
+
 
 async function solve(input: string[]): Promise<number[]> {
 
     // Note we need to filter that empty last line of the input …
-    const strategyGuide = input.map(round => /([A-C]) ([X-Z])/.exec(round) || [])
-        .filter(([_, m1, m2]) => m1 && m2)
-        .map(([_, m1, m2]) => ({ p1: p1Item(m1), p2: p2Item(m2) }));
+    const filteredInput = input.map(round => /([A-C]) ([X-Z])/.exec(round) || [])
+        .filter(([_, m1, m2]) => m1 && m2);
 
-    const totalScore = strategyGuide.reduce((score, { p1, p2 }) => score + SCORES[p1][p2] + p2, 0);
-    return [totalScore];
+    const part1StrategyGuide = filteredInput
+        .map(([_, m1, m2]) => ({ i1: p1Item(m1), i2: p2Item(m2) }));
+
+    const part1Score = part1StrategyGuide.reduce((score, { i1: p1, i2: p2 }) => score + 3 * PART1_SCORES[p1][p2] + p2, 0);
+
+    const part2StrategyGuide = filteredInput
+        .map(([_, m1, m2]) => ({ i1: p1Item(m1), s2: p2Score(m2) }));
+
+    const part2Score = part2StrategyGuide.reduce((score, { i1: p1, s2 }) => {
+        // Convert strategy guide entry to required 2up move
+        const p2 = PART2_ITEMS[p1][s2];
+        return score + 3 * PART1_SCORES[p1][p2] + p2;
+    }, 0);
+
+    return [part1Score, part2Score];
 }
 
 export default { solve };
