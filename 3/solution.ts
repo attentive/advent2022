042d34a5a858957019ea991b8cdcf648cf854512
Tom <tom@trailmarker.io>
2022-12-05 22:16:05 +0930

    solution to Day 3 Part 2

--- 3/solution.ts
+++ 3/solution.ts
@@ -1,9 +1,12 @@
 type Rucksack = {
+    whole: string[],
     first: string[],
     second: string[]
 };
 
-const getUniqueSharedItems = (rucksack: Rucksack): string[] => [...new Set(rucksack.first.filter((item) => rucksack.second.includes(item)))];
+const inBoth = (c1: string[], c2: string[]): string[] => [...new Set(c1.filter((item) => c2.includes(item)))];
+const inBothCompartments = (rucksack: Rucksack): string[] => inBoth(rucksack.first, rucksack.second);
+const inAllThreeRucksacks = ([r1, r2, r3]: Rucksack[]): string[] => inBoth(inBoth(r1.whole, r2.whole), r3.whole);
 
 const itemPriority = (itemType: string): number => {
     const code = itemType.charCodeAt(0);
@@ -14,12 +17,21 @@ const itemPriority = (itemType: string): number => {
 
 async function solve(input: string[]): Promise<number[]> {
 
-    const rucksacks = input.map((line) => ({ first: Array.from(line.slice(0, line.length / 2)), second: Array.from(line.slice(line.length / 2)) }) as Rucksack);
+    const rucksacks = input.filter(line => line)
+        .map((line) => ({
+            whole: Array.from(line),
+            first: Array.from(line.slice(0, line.length / 2)),
+            second: Array.from(line.slice(line.length / 2))
+        }) as Rucksack);
 
-    const sharedItems = rucksacks.flatMap(getUniqueSharedItems);
-
+    const sharedItems = rucksacks.flatMap(inBothCompartments);
     const part1TotalItemPriorities = sharedItems.reduce((acc, itemType) => acc + itemPriority(itemType), 0);
-    return [part1TotalItemPriorities];
+
+    const part2Groups = [...Array(rucksacks.length / 3).keys()].map((i) => rucksacks.slice(i * 3, i * 3 + 3));
+    const part2Badges = part2Groups.flatMap(inAllThreeRucksacks);
+    const part2TotalBadgePriorites = part2Badges.reduce((acc, itemType) => acc + itemPriority(itemType), 0);
+
+    return [part1TotalItemPriorities, part2TotalBadgePriorites];
 }
 
 export default { solve };
